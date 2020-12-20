9d14106f2ad64de0e5893b377fcdcfea8cfcd2e9
KeydetZ <zhz087@eng.ucsd.edu>
2020-12-19 19:35:09 -0800

    init unit and integration tests (empty)

---
+++ test/unit/unittest_pokemon.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class PokemonUnitTest(TestCase):
+    def test_filter_moves_by_generation(self):
+        self.fail()
+
+    def test_generate_poke_request(self):
+        self.fail()
+
+    def test_process_poke_request(self):
+        self.fail()
+
+    def test_handle_pokemon_response(self):
+        self.fail()
+
+    def test_handle_type_response(self):
+        self.fail()
